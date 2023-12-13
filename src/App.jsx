ac104578f171cf441d8b43d184c59f6d755a93c7
mauperez9918 <mauperez9918@gmail.com>
2023-12-13 10:10:59 -0300

    ADD Fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="text-7xl font-bodoni">
+      MATILDE funciona desde 2017, caracterizándose por básicos atemporales,
+      prendas únicas, entre otros. Te invito a visitar mi tienda y me va a
+      encantar asesorarte.
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
+    fontFamily: {
+      "bodoni": ["Bodoni Moda"],
+      "workSans": ["Work Sans"],
+      sans: ['"Ruda"', defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
