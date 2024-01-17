9d9221a80298100d0d5e0dbca5fd1b60f00bea0a
Anna Esteve <aesteve8@gmail.com>
2024-01-17 19:17:36 +0100

    CREATE accordion components

--- src/components/Sections/Accordion.jsx
+++ src/components/Sections/Accordion.jsx
@@ -10,34 +10,58 @@ const Accordion = ({ section }) => {
   };
 
   return (
-    <details
-      name="info"
-      className={`p-4 ${section.color} section`}
-      onClick={toggleAccordion}
-    >
-      <summary className="list-none">
-        <div className="px-4 py-2 flex flex-col gap-2 h-56 justify-between">
-          <div className="flex flex-col border-b border-black text-2xl gap-2 pb-2 w-3/4">
-            <span>{section.number}</span>
-            <span>{section.name}</span>
-          </div>
+    <div className={`${isOpen ? "min-h-[600px]" : "min-h-[230px]"} p-4 ${section.color} accordion`}>
+      <div
+        className="px-4 py-2 flex flex-col gap-2 h-56 justify-between title"
+        onClick={toggleAccordion}
+      >
+        <div className="flex flex-col border-b border-black text-2xl gap-2 pb-2 w-3/4">
+          <span>{section.number}</span>
+          <span>{section.name}</span>
+        </div>
 
-          <div className="flex self-end items-center gap-4 pr-6 py-2">
-            <span className="text-sm">{isOpen ? "Ver menos" : "Ver más"}</span>
-            <div className={isOpen ? "rotate-180" : ""}>
-              <Arrow />
-            </div>
+        <div className="flex self-end items-center gap-4 pr-6 py-2">
+          <span className="text-sm">{isOpen ? "Ver menos" : "Ver más"}</span>
+          <div className={isOpen ? "rotate-180" : ""}>
+            <Arrow />
           </div>
         </div>
-      </summary>
-      <div className="section-info">
-        <p
-          className="mb-8 section-info-text"
-          dangerouslySetInnerHTML={{ __html: section.text }}
-        />
-        <img src="" alt="" />
+
+        {isOpen && (
+          <div className="h-[100px] section-info">
+            <p dangerouslySetInnerHTML={{ __html: section.text }} />
+          </div>
+        )}
       </div>
-    </details>
+    </div>
+    // <details
+    //   name="info"
+    //   className={`p-4 ${section.color} section`}
+    //   onClick={toggleAccordion}
+    // >
+    //   <summary className="list-none">
+    //     <div className="px-4 py-2 flex flex-col gap-2 h-56 justify-between">
+    //       <div className="flex flex-col border-b border-black text-2xl gap-2 pb-2 w-3/4">
+    //         <span>{section.number}</span>
+    //         <span>{section.name}</span>
+    //       </div>
+
+    //       <div className="flex self-end items-center gap-4 pr-6 py-2">
+    //         <span className="text-sm">{isOpen ? "Ver menos" : "Ver más"}</span>
+    //         <div className={isOpen ? "rotate-180" : ""}>
+    //           <Arrow />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </summary>
+    //   <div className="section-info">
+    //     <p
+    //       className="mb-8 section-info-text"
+    //       dangerouslySetInnerHTML={{ __html: section.text }}
+    //     />
+    //     <img src="" alt="" />
+    //   </div>
+    // </details>
   );
 };
 
