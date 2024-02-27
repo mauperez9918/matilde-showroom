cdaebb21622a1c1bc2758b4e820fe282b0b9fe98
Anna Esteve <aesteve8@gmail.com>
2024-02-27 20:56:54 +0100

    MOD AccordionInner image size

--- src/components/Sections/AccordionInner.jsx
+++ src/components/Sections/AccordionInner.jsx
@@ -13,12 +13,12 @@ const AccordionInner = ({ sectionName, text, innerColor, media }) => {
 
   const ColoredWppIcon = () => {
     const isBlack = screenWidth < 768 ? "black" : "white";
-    return <WhatsappIcon color={isBlack} size={25} />;
+    return <WhatsappIcon color={isBlack} iconSize={25} />;
   };
 
   const ColoredInstagramIcon = () => {
     const isBlack = screenWidth < 768 ? "black" : "white";
-    return <InstagramIcon color={isBlack} size={25} />;
+    return <InstagramIcon color={isBlack} iconSize={25} />;
   };
 
   const renderContent = () => {
@@ -40,14 +40,9 @@ const AccordionInner = ({ sectionName, text, innerColor, media }) => {
       case "Contacto":
         return (
           <>
-            <div className="w-full font-workSans text-sm md:text-lg lg:text-xl">
-              <p
-                dangerouslySetInnerHTML={{ __html: text }}
-                className="lg:w-[400px]"
-              />
-            </div>
-            <div className="w-full flex flex-col xl:flex-row">
-              <div className="w-full lg:w-[400px] xl:w-1/2 flex flex-col md:items-center xl:items-start xl:mt-8">
+            <div className="w-full lg:w-[400px] xl:w-1/2 font-workSans text-sm md:text-lg lg:text-xl">
+              <p dangerouslySetInnerHTML={{ __html: text }} />
+              <div className="w-full mt-4 flex flex-col md:items-center xl:items-start ">
                 <p className="text-sm md:text-lg lg:text-xl">Encontranos en:</p>
                 <div className="my-4 flex justify-between gap-6">
                   <WhatsappButton
@@ -66,13 +61,9 @@ const AccordionInner = ({ sectionName, text, innerColor, media }) => {
                   </InstagramButton>
                 </div>
               </div>
-              <div className="w-full lg:w-[400px] xl:w-1/2 z-20 lg:self-start mt-8 xl:-mt-20 xl:-ml-20">
-                <img
-                  src={media}
-                  alt=""
-                  className="mx-auto md:w-2/3 lg:w-auto"
-                />
-              </div>
+            </div>
+            <div className="w-full lg:w-1/2 p-4 md:p-0 z-20">
+              <img src={media} alt="" className="mx-auto xl:w-3/4" />
             </div>
           </>
         );
@@ -98,7 +89,7 @@ const AccordionInner = ({ sectionName, text, innerColor, media }) => {
   };
 
   return (
-    <div className="accordion-info relative p-4 md:pt-4 md:pb-12 md:px-16 lg:pt-[115px] lg:pl-[115px] lg:pb-16 flex flex-col lg:flex-row lg:flex-wrap items-start gap-4 md:gap-8 lg:gap-8 xl:gap-0 animate__animated animate__fadeIn ">
+    <div className="accordion-info relative p-4 md:pt-4 md:pb-12 md:px-16 lg:p-20 xl:pt-[115px] xl:pl-[115px] lg:pb-16 flex flex-col lg:flex-row lg:flex-wrap items-start gap-4 md:gap-8 lg:gap-8 xl:gap-0 animate__animated animate__fadeIn ">
       {renderContent()}
       <div
         className={`z-10 absolute bottom-0 right-0 lg:w-1/4 lg:h-full ${innerColor}`}
